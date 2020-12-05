4ee5b44342864f1e476ac425de53d368a9b70a04
panxubai <margin100px@163.com>
2020-12-05 16:19:41 +0800

    src

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale,Arial;
   text-align: center;
   color: #2c3e50;
   height: 100%;
